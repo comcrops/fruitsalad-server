39655e5c6bf4863249ed5fe445ab39fd62c6ecc5
s-prechtl <stefan@tague.at>
2024-04-10 10:06:43 +0200

    feat: added db connection

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
-	_ "fmt"
 	. "fruitsalad-server/model"
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type MyStruct struct {
@@ -27,9 +31,32 @@ type Guess struct {
 }
 
 func main() {
+	db, err := sql.Open("postgres", loadDatabaseUrl())
+
+	if err != nil {
+		log.Fatalf("Error while connecting to db")
+	}
+	defer db.Close()
+
 	http.HandleFunc("/game/new", generateRandomGame)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func loadDatabaseUrl() string {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Printf("Error loading .env file")
+	}
+
+	connectionString := os.Getenv("DATABASE_URL")
+
+	if connectionString == "" {
+		log.Printf("Connection string wasn't found or is empty!")
+	}
+	return connectionString
+}
+
 
 func generateRandomGame(w http.ResponseWriter, req *http.Request) {
 	value := GetRandomRgbValue()
